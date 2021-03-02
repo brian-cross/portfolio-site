a7dd71f800154cb35d4a8a9c0cdaf3c616b072ed
Brian Cross <brian.cross.1977@gmail.com>
2021-03-02 14:31:59 -0700

    Added primary darkened color

--- styles/theme.js
+++ styles/theme.js
@@ -1,9 +1,11 @@
 const primary = "hsl(215, 15%, 50%)";
+const primaryDarkened = "hsl(215, 15%, 33%)";
 
 const theme = {
   colors: {
     siteBackground: `linear-gradient(180deg,hsl(215, 100%, 10%) 0%,hsl(215, 50%, 20%) 100%)`,
     primary,
+    primaryDarkened,
     scrollbar: {
       thumb: primary,
       track: "hsl(215, 15%, 20%)",
