f81ed2380913ba9e62ec60b0c543b6fbaab23bd9
Brian Cross <brian.cross.1977@gmail.com>
2021-03-02 18:10:37 -0700

    Changed variable names

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,11 @@ import Header from "../components/Header";
 import Home from "../components/Home";
 
 export default function Main() {
-  const [homeComplete, setHomeComplete] = useState(false);
+  const [homeIntroComplete, setHomeIntroComplete] = useState(false);
   return (
     <>
-      <Header animateIn={homeComplete} />
-      <Home onSplashComplete={() => setHomeComplete(true)} />
+      <Header animateIn={homeIntroComplete} />
+      <Home onIntroComplete={() => setHomeIntroComplete(true)} />
     </>
   );
 }
