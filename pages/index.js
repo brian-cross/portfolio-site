b322fe513b6cd76658f852ef6d80574c319d7c3d
Brian Cross <brian.cross.1977@gmail.com>
2021-03-03 18:48:54 -0700

    Add new page components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import Header from "../components/Header";
 import Home from "../components/Home";
+import About from "../components/About";
+import Work from "../components/Work";
+import Contact from "../components/Contact";
 
 export default function Main() {
   const [homeIntroComplete, setHomeIntroComplete] = useState(false);
@@ -8,6 +11,9 @@ export default function Main() {
     <>
       <Header animateIn={homeIntroComplete} />
       <Home onIntroComplete={() => setHomeIntroComplete(true)} />
+      <Work />
+      <About />
+      <Contact />
     </>
   );
 }
