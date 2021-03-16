a65129c69d8b4f5efa8aa8c8e222e4c66ca5a82f
Brian Cross <brian.cross.1977@gmail.com>
2021-03-16 14:09:57 -0600

    Changed font weight

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com"></link>
         <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;800&display=swap"
           rel="stylesheet"
         ></link>
       </Head>
