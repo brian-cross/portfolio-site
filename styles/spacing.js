3cfdb354db2e289d7d1b9bea5b6accfef27b3660
Brian Cross <brian.cross.1977@gmail.com>
2021-03-18 15:34:23 -0600

    Added portfolio image width

--- styles/spacing.js
+++ styles/spacing.js
@@ -3,6 +3,7 @@ const spacing = {
   page: "height: 100%; width: 90%; margin: 0 auto;",
   headerIconWidth: "clamp(2.75rem, 4vw, 3.5rem)",
   scrollbarWidth: "6px",
+  portfolioImageWidth: "700px",
 };
 
 export default spacing;
