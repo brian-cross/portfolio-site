54669f37299498b2b43df0e33dd23ccecbd75de5
Brian Cross <brian.cross.1977@gmail.com>
2021-03-18 15:31:34 -0600

    Styling changes

--- components/Contact.js
+++ components/Contact.js
@@ -1,11 +1,13 @@
 import useAnimatedHeading from "../hooks/useAnimatedHeading";
 import spacing from "../styles/spacing";
+import PageSeparator from "./PageSeparator";
 
 export default function Contact() {
   const ref = useAnimatedHeading();
   return (
     <>
       <section className="container contact">
+        <PageSeparator />
         <h1 ref={ref}>Get in touch</h1>
       </section>
       <style jsx>
@@ -13,6 +15,12 @@ export default function Contact() {
           .container {
             ${spacing.page}
             min-height: 100vh;
+            position: relative;
+            padding: 6rem 0;
+
+            h1 {
+              text-align: right;
+            }
           }
         `}
       </style>
