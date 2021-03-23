2a68bb86c165468bb8d7f9d2f3238bcaa39d66cb
Brian Cross <brian.cross.1977@gmail.com>
2021-03-23 11:56:05 -0600

    Added animation

--- components/PageSeparator.js
+++ components/PageSeparator.js
@@ -1,9 +1,40 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { useEffect, useRef } from "react";
 import theme from "../styles/theme";
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function PageSeparator() {
+  const ref = useRef();
+
+  useEffect(() => {
+    const target = ref.current;
+
+    gsap.set(target, { scaleX: 0, transformOrigin: "right" });
+
+    ScrollTrigger.create({
+      trigger: target,
+      start: "top bottom",
+      end: "top top",
+      onEnter: show,
+      onLeave: hide,
+      onEnterBack: show,
+      onLeaveBack: hide,
+    });
+
+    function show() {
+      gsap.to(target, { scaleX: 1, ease: "power3.out", duration: 2 });
+    }
+
+    function hide() {
+      gsap.to(target, { scaleX: 0 });
+    }
+  }, []);
+
   return (
     <>
-      <div />
+      <div ref={ref} />
       <style jsx>
         {`
           div {
@@ -12,7 +43,7 @@ export default function PageSeparator() {
             position: absolute;
             left: -5vw;
             top: 0;
-            background: ${theme.colors.primary};
+            background: ${theme.colors.mediumDark};
           }
         `}
       </style>
