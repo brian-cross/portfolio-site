ce2ff89388b98ad11428800c25539709d75b2be6
Brian Cross <brian.cross.1977@gmail.com>
2021-03-25 22:17:21 -0600

    Changed section backgrounds to text

--- components/BackgroundWrapper.js
+++ components/BackgroundWrapper.js
@@ -8,12 +8,12 @@ export default function BackgroundWrapper({ children }) {
   const ref = useRef();
 
   useEffect(() => {
-    gsap.set(ref.current, { yPercent: 15 });
-    gsap.to(ref.current, {
-      yPercent: -15,
+    const target = ref.current;
+    gsap.to(target, {
+      yPercent: -100,
       ease: "none",
       scrollTrigger: {
-        trigger: ref.current,
+        trigger: target,
         scrub: 1,
       },
     });
@@ -23,7 +23,7 @@ export default function BackgroundWrapper({ children }) {
     <>
       <div className="wrapper">
         <div className="parallax" ref={ref}>
-          {children}
+          <span>{children}</span>
         </div>
       </div>
       <style jsx>
@@ -34,9 +34,9 @@ export default function BackgroundWrapper({ children }) {
             position: absolute;
             visibility: hidden;
             overflow: hidden;
-            width: 100vw;
+            width: 100%;
             height: 100%;
-            left: -5vw;
+            //left: -5vw;
             top: 0;
             z-index: -1;
 
@@ -47,7 +47,24 @@ export default function BackgroundWrapper({ children }) {
             .parallax {
               flex-grow: 1;
               height: 100%;
-              max-height: 100vh;
+              //max-height: 100vh;
+
+              span {
+                margin: 1em 0;
+                line-height: 0.8;
+                font-size: 20vw;
+                font-weight: bold;
+                white-space: nowrap;
+                text-transform: uppercase;
+                -webkit-text-stroke: 0.01em currentColor;
+                -webkit-text-fill-color: rgba(0, 0, 0, 0);
+                opacity: 0.05;
+                position: absolute;
+                top: 0.1em;
+                left: 100%;
+                transform-origin: top left;
+                transform: rotate(90deg);
+              }
             }
           }
         `}
