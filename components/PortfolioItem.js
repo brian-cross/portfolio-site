ba1f6ac8f6b7a980a18d5b69f166c480ef8ed139
Brian Cross <brian.cross.1977@gmail.com>
2021-03-26 21:32:09 -0600

    Layout change

--- components/PortfolioItem.js
+++ components/PortfolioItem.js
@@ -46,16 +46,11 @@ export default function PortfolioItem({
         {`
           .portfolio-item {
             display: flex;
+            align-items: flex-start;
             flex-direction: column;
-            align-items: stretch;
-            justify-content: space-between;
-            @media screen and (min-width: 1000px) {
-              flex-direction: row;
-            }
 
             .graphic {
               position: relative;
-              flex-shrink: 1;
 
               h2 {
                 margin: 0;
@@ -63,7 +58,7 @@ export default function PortfolioItem({
                 .heading {
                   position: absolute;
                   left: 5%;
-                  top: -0.6em;
+                  top: -1em;
                   z-index: 1;
                   padding: 0 0.5em;
                   border-radius: 0.25em;
@@ -87,18 +82,11 @@ export default function PortfolioItem({
             }
 
             .text {
-              flex-shrink: 1;
               margin-top: 5vh;
               max-width: ${spacing.portfolioImageWidth};
               display: flex;
               flex-direction: column;
               justify-content: space-evenly;
-              @media screen and (min-width: 1000px) {
-                flex-grow: 1;
-                flex-basis: 40rem;
-                margin-top: 0;
-                margin-left: 5vw;
-              }
 
               .link {
                 font-weight: bold;
