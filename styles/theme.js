b63a522126e553f7fb39c3ddb105d0a8cae2b236
Brian Cross <brian.cross.1977@gmail.com>
2021-03-30 08:22:09 -0600

    Changed background colors

--- styles/theme.js
+++ styles/theme.js
@@ -4,12 +4,16 @@ const white = "white";
 const lightGrey = "hsl(200, 25%, 75%)";
 const mediumDark = "hsl(215, 15%, 25%)";
 const menuBackground = "#000911";
-const siteBackground = "#012";
+const siteBackground = "hsl(219, 41%, 13%)";
+const sectionBgMain = `linear-gradient(80deg, hsl(219, 41%, 13%), hsl(212, 40%, 24%))`;
+const sectionBgAbout = `linear-gradient(80deg, hsl(219, 41%, 26%), hsl(212, 40%, 48%))`;
 
 const theme = {
   colors: {
     htmlBackground: siteBackground,
     siteBackground,
+    sectionBgMain,
+    sectionBgAbout,
     primary,
     accent,
     white,
