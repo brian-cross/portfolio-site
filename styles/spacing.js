0f729ec63e6aa14cd5e88e322a5964bef6992d88
Brian Cross <brian.cross.1977@gmail.com>
2021-03-30 08:22:34 -0600

    Changed global page width

--- styles/spacing.js
+++ styles/spacing.js
@@ -1,6 +1,6 @@
 const spacing = {
-  pageWidth: "90%",
-  page: "width: 90%; margin: 0 auto;",
+  pageWidth: "100%",
+  page: "width: 100%; margin: 0 auto;",
   headerIconWidth: "clamp(2.75rem, 4vw, 3.5rem)",
   scrollbarWidth: "6px",
   portfolioImageWidth: "700px",
