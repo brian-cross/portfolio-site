e2a405e9dbd54d9145eb036f97c4ab32a1e89ba2
Brian Cross <brian.cross.1977@gmail.com>
2021-04-01 12:57:10 -0600

    Removed social icons (now in footer) and added logic for dimming / showing the menu icon

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,21 @@
 import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import GithubIcon from "./GithubIcon";
-import CodePenIcon from "./CodePenIcon";
-import TwitterIcon from "./TwitterIcon";
 import MenuIcon from "./MenuIcon";
 import Menu from "../components/Menu";
 import theme from "../styles/theme";
-// import ScrollIndicator from "./ScrollIndicator";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Header({ animateIn, onVisible }) {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
   const menuIcon = useRef();
-  const socialIcons = useRef();
 
   useEffect(() => {
     if (!animateIn) return;
-    gsap.from([menuIcon.current, socialIcons.current], {
+    gsap.from(menuIcon.current, {
       scale: 1.25,
       yPercent: 10,
       ease: "power1.inOut",
@@ -34,41 +30,51 @@ export default function Header({ animateIn, onVisible }) {
       trigger: "window",
       start: "5% top",
       end: "+=5%",
-      onLeave: () => {
-        gsap.to([menuIcon.current, socialIcons.current], {
-          autoAlpha: 0,
-          yPercent: -50,
-          ease: "power3.out",
-          duration: 1,
-        });
-      },
-      onLeaveBack: () => {
-        gsap.to([menuIcon.current, socialIcons.current], {
-          autoAlpha: 1,
-          yPercent: 0,
-          ease: "power3.out",
-          duration: 1,
-        });
-      },
+      onLeave: () => setScrolled(true),
+      onLeaveBack: () => setScrolled(false),
     });
   }, []);
+
+  function handleMouseEnter() {
+    if (scrolled) showIcon(0.3);
+  }
+
+  function handleMouseLeave() {
+    if (scrolled && !menuOpen) dimIcon(0.3);
+  }
+
+  function handleClick() {
+    setMenuOpen(prev => !prev);
+  }
+
+  function dimIcon(duration = 1) {
+    gsap.to(menuIcon.current, {
+      opacity: 0.2,
+      ease: "none",
+      duration,
+    });
+  }
+
+  function showIcon(duration = 1) {
+    gsap.to(menuIcon.current, {
+      opacity: 1,
+      ease: "none",
+      duration,
+    });
+  }
 
   return (
     <>
       <header>
         <div className="container">
           <Menu isOpen={menuOpen} />
-          <div className="social-icons" ref={socialIcons}>
-            <TwitterIcon />
-            <CodePenIcon />
-            <GithubIcon />
-          </div>
           <MenuIcon
             open={menuOpen}
             ref={menuIcon}
-            onClick={() => setMenuOpen(prev => !prev)}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            onClick={handleClick}
           />
-          {/* {enableScroll ? <ScrollIndicator /> : null} */}
         </div>
       </header>
       <style jsx>
@@ -81,14 +87,8 @@ export default function Header({ animateIn, onVisible }) {
             color: ${theme.colors.lightGrey};
 
             .container {
-              display: flex;
-              justify-content: space-between;
               width: 90%;
               margin: 2.5vw auto;
-
-              .social-icons {
-                visibility: hidden;
-              }
             }
           }
         `}
