6ac4d72e5d52ef42a9a34d4403988930988a00e2
Brian Cross <brian.cross.1977@gmail.com>
2021-04-01 13:00:28 -0600

    Changed name of const from headerIconWidth to socialIconWidth

--- styles/spacing.js
+++ styles/spacing.js
@@ -1,7 +1,7 @@
 const spacing = {
   pageWidth: "100%",
   page: "width: 100%; margin: 0 auto;",
-  headerIconWidth: "clamp(2.75rem, 4vw, 3.5rem)",
+  socialIconWidth: "clamp(2.75rem, 4vw, 3rem)",
   scrollbarWidth: "6px",
   portfolioImageWidth: "700px",
 };
